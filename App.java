c56ede6287af95023d7a5a9bfa81d5c52629a3b7
Fawiboi06 <fawaz.iqbal@elev.ga.ntig.se>
2026-03-27 10:03:42 +0100

    Menu. add 'Subtract' + choice prompt

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     static void run(){
-        System.out.pirntln("1)add\n0) Exit");
-        Sytem.out.println("Choice:");
+        System.out.pirntln("1)subtract\n0) Exit");
+        Sytem.out.println("Your choice:");
         String choice = new java.util.Scanner(System.in).nextLine();
 }
