74aa7ce9b2c4a52981cb8df7d6bfc3720a9af9fa
Fawiboi06 <fawaz.iqbal@elev.ga.ntig.se>
2026-03-27 10:23:42 +0100

    include Subtract

--- App.java
+++ App.java
@@ -1,6 +1,6 @@
 public class App {
     static void run(){
-        System.out.pirntln("1)subtract\n0) Exit");
+        System.out.pirntln("1) Add\n2)subtract\n0) Exit");
         Sytem.out.println("Your choice:");
         String choice = new java.util.Scanner(System.in).nextLine();
 }
