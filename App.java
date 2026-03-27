7a277779509fd4762485ce52c7161fe5acf65669
Haroon <haroonahmed050704@gmail.com>
2026-03-27 09:56:50 +0100

    Menu: add 'Add' + choice prompt

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     static void run(){
-        System.out.println("Calculator v.0.1");
-    }
+        System.out.pirntln("1)add\n0) Exit");
+        Sytem.out.println("Choice:");
+        String choice = new java.util.Scanner(System.in).nextLine();
 }
