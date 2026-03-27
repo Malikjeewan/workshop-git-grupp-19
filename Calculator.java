affa011ba0919fe5faf7f999fba974c7e8af752f
Fawiboi06 <fawaz.iqbal@elev.ga.ntig.se>
2026-03-27 11:05:46 +0100

    Bug: make sub() wrong on purpose

--- Calculator.java
+++ Calculator.java
@@ -8,6 +8,7 @@ public class Calculator {
     public static double mul(double a, double b) { return a * b;}
 
     public static double sub(double a, double b) { return a - b;}
+    public static double sub(double a, double b) { return a + b}
     /**Return the quotient of a and b. */
     public static double div(double a,double b){return a/b;}
 
