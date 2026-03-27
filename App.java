1394681b452f1b2735cf0b5b529092db5ff0022d
Malikjeewan <jeewanmalik27@gmail.com>
2026-03-27 09:35:19 +0100

    Init: add App.java with Run() + README hint

---
+++ App.java
@@ -0,0 +1,5 @@
+public class App {
+    static void run(){
+        System.out.println("Calculator v.0.1");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # workshop-git-grupp-19
+
+'javac App.java && java App' (main method will be added later)
