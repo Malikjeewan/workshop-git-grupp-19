1a02d44304503b3dbabd805892ccb4393879ca8d
Malikjeewan <jeewanmalik27@gmail.com>
2026-03-27 10:37:22 +0100

    Handle choice 1 stub

--- App.java
+++ App.java
@@ -3,5 +3,6 @@ public class App {
         System.out.pirntln("1) Add\n2)subtract\n3) Multiply\n0) Exit");
         Sytem.out.println("Your choice:");
         String choice = new java.util.Scanner(System.in).nextLine();
-        if("1".equals(choice)) System.out.println(calculator.add(0,0));
+        if("1".equals(choice)) System.out.println("TODO Add");
+    }
 }
