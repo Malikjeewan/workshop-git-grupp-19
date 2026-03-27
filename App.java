ee2ff7d38fa0cc8d0624de1cb93ede428e22c23d
Haroon <haroonahmed050704@gmail.com>
2026-03-27 10:18:40 +0100

    Use Calculator.add() placeholder in App

--- App.java
+++ App.java
@@ -3,4 +3,5 @@ public class App {
         System.out.pirntln("1)subtract\n0) Exit");
         Sytem.out.println("Your choice:");
         String choice = new java.util.Scanner(System.in).nextLine();
+        if("1".equals(choice)) System.out.println(calculator.add(0,0));
 }
